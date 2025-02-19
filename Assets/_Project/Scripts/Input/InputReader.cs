16a21d0a847c5dd64d3a0846ee270265ca41727f
Paul Gush <Paul1208Gush@Gmail.com>
2025-02-19 00:49:23 +0000

    Client-side input, cinemachine package

--- Assets/_Project/Scripts/Input/InputReader.cs
+++ Assets/_Project/Scripts/Input/InputReader.cs
@@ -1,19 +1,79 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static InputSystem_Actions;
 
 namespace _Project.Scripts.Input
 {
-    public class InputReader : MonoBehaviour
+    public interface IInputReader
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        Vector2 Direction { get; }
+        void EnablePlayerActions();
+    }
+    
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Input/InputReader")]
+    public class InputReader : ScriptableObject, IInputReader, IPlayerActions
+    {
+        public UnityAction<Vector2> Move = delegate {  };
+        public UnityAction<bool> Jump = delegate {  };
 
-        // Update is called once per frame
-        void Update()
-        {
+        public InputSystem_Actions InputActions;
         
+        public Vector2 Direction => InputActions.Player.Move.ReadValue<Vector2>();
+        
+        public bool IsJumpKeyPressed => InputActions.Player.Jump.IsPressed();
+        public void EnablePlayerActions()
+        {
+            if (InputActions == null)
+            {
+                InputActions = new InputSystem_Actions();
+                InputActions.Player.SetCallbacks(this);
+            }
+            InputActions.Enable();
+        }
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Move?.Invoke(context.ReadValue<Vector2>());
+        }
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            
+        }
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            
+        }
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            
+        }
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+            
+        }
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            switch(context.phase)
+            {
+                case InputActionPhase.Started:
+                    Jump?.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    Jump?.Invoke(false);
+                    break;
+            }
+        }
+        public void OnPrevious(InputAction.CallbackContext context)
+        {
+            
+        }
+        public void OnNext(InputAction.CallbackContext context)
+        {
+            
+        }
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            
         }
     }
 }
--- Assets/_Project/Scripts/PlayableCharacter.cs
+++ Assets/_Project/Scripts/PlayableCharacter.cs
@@ -1,15 +1,45 @@
+using _Project.Scripts.Input;
 using Mirror;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _Project.Scripts
 {
     public class PlayableCharacter : NetworkBehaviour
     {
+        [SerializeField] private InputReader m_inputReader;
         public override void OnStartLocalPlayer()
         {
             base.OnStartLocalPlayer();
             
+            if (this.isOwned)
+            {
+                m_inputReader.EnablePlayerActions();
+                EventSubscriptions();
+                
+                Debug.Log(name + " : I own myself!");
+            }
+            
             Debug.Log("I am the local player");
+        }
+
+        [Client]
+        private void EventSubscriptions()
+        {
+            m_inputReader.Move += OnMove;
+            m_inputReader.Jump += OnJump;
+        }
+        
+        [Client]
+        private void OnJump(bool arg0)
+        {
+            Debug.Log(name + " :I am listening to the jump event");
+        }
+        
+        [Client]
+        private void OnMove(Vector2 arg0)
+        {
+            Debug.Log(name + " :I am listening to the move event");
         }
     }
 }
