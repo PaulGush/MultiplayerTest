ad11993b1281bc6668bea4b61c4b9ad97e4dd6c5
Paul Gush <Paul1208Gush@Gmail.com>
2025-02-20 00:20:41 +0000

    Update CharacterAnimator.cs

--- Assets/CharacterAnimator.cs
+++ Assets/CharacterAnimator.cs
@@ -12,49 +12,12 @@ public class CharacterAnimator : NetworkBehaviour
     private static readonly int Jump = Animator.StringToHash("Jump");
     private static readonly int Grounded = Animator.StringToHash("Grounded");
     
-    [SyncVar] private float m_speed;
-    [SyncVar] private float m_motionSpeed;
-    [SyncVar] private bool m_jump;
-    [SyncVar] private bool m_grounded;
-    
     private void Update()
     {
         if (isOwned)
         {
             UpdateLocalAnimatorWithLocalInput();
         }
-        
-        /*if (isServer && isOwned)
-        {
-            RPC_LocalPlayerInputToServer();
-        }
-        
-        if (isServer && !isOwned)
-        {
-            RPC_ServerUpdateToClients();
-        }*/
-    }
-
-    [ClientRpc]
-    private void RPC_LocalPlayerInputToServer()
-    {
-        //If this character is owned by the local player, then we update the server with the local player's input
-        
-        m_speed = m_inputReader.Direction.magnitude * 6;
-        m_motionSpeed = m_inputReader.Direction.magnitude;
-        m_jump = m_inputReader.IsJumpKeyPressed;
-        m_grounded = true;
-    }
-    
-    [ClientRpc]
-    private void RPC_ServerUpdateToClients()
-    {
-        //If this character is not owned by the local player, then we update the clients with the whatever the server last stored as input from that player 
-        
-        m_animator.SetFloat(Speed, m_speed);
-        m_animator.SetFloat(MotionSpeed, m_motionSpeed);
-        m_animator.SetBool(Jump, m_jump);
-        m_animator.SetBool(Grounded, m_grounded);
     }
 
     [Client]
