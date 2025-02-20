e7892d3459c098e11aecef59458f54ebdb0f0d12
Paul Gush <Paul1208Gush@Gmail.com>
2025-02-20 20:50:50 +0000

    Camera

--- Assets/_Project/Scripts/CharacterLocomotion.cs
+++ Assets/_Project/Scripts/CharacterLocomotion.cs
@@ -1,7 +1,8 @@
-using System;
 using _Project.Scripts.Input;
 using Mirror;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Project.Scripts
 {
@@ -9,7 +10,24 @@ namespace _Project.Scripts
     {
         [SerializeField] private InputReader m_inputReader;
         [SerializeField] private float m_moveSpeed = 5f;
+        [SerializeField] private float m_rotationVelocity;
         
+        [Tooltip("How fast the character turns to face movement direction")]
+        [Range(0.0f, 0.3f)]
+        [SerializeField] private float m_rotationSmoothTime = 0.12f;
+
+        [SerializeField] private CinemachineCamera m_camera;
+        
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+
+            if (!isOwned)
+            {
+                m_camera.gameObject.SetActive(false);
+            }
+        }
+
         private void Update()
         {
             if (isOwned)
@@ -25,6 +43,28 @@ namespace _Project.Scripts
             Vector2 direction = m_inputReader.Direction;
             Vector3 move = new Vector3(direction.x, 0, direction.y);
             transform.Translate(move * (Time.deltaTime * m_moveSpeed), Space.World);
+            
+            // if there is a move input rotate player when the player is moving
+            if (!Equals(m_inputReader.Move, Vector2.zero))
+            {
+                float targetRotation;
+
+                if (isOwned)
+                {
+                    targetRotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg +
+                                      m_camera.transform.eulerAngles.y;
+                }
+                else
+                {
+                    targetRotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+                }
+
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref m_rotationVelocity,
+                    m_rotationSmoothTime);
+
+                // rotate to face input direction relative to camera position
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
         }
     }
 }
